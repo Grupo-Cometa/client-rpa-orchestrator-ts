e3d9c679b0c4200614ae709dfe04537c77f6cbde
Pedro Souza <pedroh.olives@gmail.com>
2023-09-14 15:12:27 -0400

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Log } from './Log';
 export { Status } from './Status';
 export { Triggers } from './Triggers';
 export { Bootstrap } from './Bootstrap';
+export { InterfaceMain } from './InterfaceMain';
