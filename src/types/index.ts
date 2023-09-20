8133f7afcb6fd92d856ded58ea35c2d4d991fff4
Pedro Souza <pedroh.olives@gmail.com>
2023-09-20 14:18:04 -0400

    feat: adiciona envio do token para execução

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,7 +9,7 @@ type Execution = {
     type: 'execution',
     date: string,
     status: "START" | 'STOP',
-    token: string,
+    token?: string,
     schedule_id: string,
     parameters: string,
     public_id: string | undefined
