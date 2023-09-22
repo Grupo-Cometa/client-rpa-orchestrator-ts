1d658477583faacd2bdd2ecd2d195c6d9f0a890d
Lucas Mota <geraldo.oliveira@viacometa.com.br>
2023-09-22 14:19:01 -0400

    fix: fiz nada

--- src/Amqp/RabbitMqServer.ts
+++ src/Amqp/RabbitMqServer.ts
@@ -2,33 +2,34 @@ import { Channel, connect, Connection } from "amqplib";
 
 export class RabbitMQServer {
 
-  private conn!: Connection;
-  private channel!: Channel;
+  // private conn!: Connection;
+  // private channel!: Channel;
 
   constructor(private uri: string) { }
 
-  private async connect(): Promise<void> {
-    this.conn = await connect(this.uri);
-    this.channel = await this.conn.createChannel();
-  }
+  // private async connect(): Promise<void> {
+  //   this.conn = await connect(this.uri);
+  //   this.channel = await this.conn.createChannel();
+  // }
 
-  public async disconnect(): Promise<void> {
-    await this.channel?.close();
-    await this.conn?.close();
-  }
+  // public async disconnect(): Promise<void> {
+  //   await this.channel?.close();
+  //   await this.conn?.close();
+  // }
 
   public async publish(queue: string, message: string): Promise<void> {
-    await this.connect();
-    await this.channel?.assertQueue(queue, {
+    const conn = await connect(this.uri);
+    const channel = await conn.createChannel()
+
+    await channel.assertQueue(queue, {
       durable: true
     });
 
-    await new Promise<void>((resolve) => {
-      this.channel?.sendToQueue(queue, Buffer.from(message));
-      setTimeout(() => {
-        resolve();
-        this.disconnect();
-      }, 500);
-    });
+    channel.sendToQueue(queue, Buffer.from(message));
+
+    setTimeout(function () {
+      conn.close();
+    }, 500);
+
   }
 }
