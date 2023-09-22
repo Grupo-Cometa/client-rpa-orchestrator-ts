fe7cb0f4634d77c44f2ab3ab5dd65f483311b804
Pedro Souza <pedroh.olives@gmail.com>
2023-09-22 15:05:30 -0400

    FEAT: 1.0.23

--- lib/Amqp/RabbitMqServer.d.ts
+++ lib/Amqp/RabbitMqServer.d.ts
@@ -1,9 +1,5 @@
 export declare class RabbitMQServer {
     private uri;
-    private conn;
-    private channel;
     constructor(uri: string);
-    private connect;
-    disconnect(): Promise<void>;
     publish(queue: string, message: string): Promise<void>;
 }
--- lib/Start.d.ts
+++ lib/Start.d.ts
@@ -3,6 +3,7 @@ declare class Start {
     private main;
     constructor(main: InterfaceMain);
     execute(sheduleId?: string, token?: string): Promise<void>;
+    private sleep;
     private getExecution;
 }
 export { Start };
