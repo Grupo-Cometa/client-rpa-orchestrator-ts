5cfe4dc5a9e862bd34e13971e08430f71b4c4c90
Pedro Souza <pedroh.olives@gmail.com>
2023-10-11 10:59:46 -0400

    CHORE: Update

--- src/Services/resendSchedules.ts
+++ src/Services/resendSchedules.ts
@@ -2,8 +2,12 @@ import { orquestrador } from "../Http/Orquestrador";
 
 async function resendSchedules() 
 {
-    const robotId = await orquestrador.getRobotId(process.env.PUBLIC_ID!);
-    await orquestrador.resendSchedules(robotId);
+    try {
+        const robotId = await orquestrador.getRobotId(process.env.PUBLIC_ID!);
+        await orquestrador.resendSchedules(robotId);
+    } catch(error: unknown) {
+
+    }
 }
 
 export { resendSchedules }
