c148720b57955173d4bf29a682d660518a618121
Pedro Souza <pedroh.olives@gmail.com>
2023-11-16 14:44:53 -0400

    FEAT: 1.0.33

--- lib/Amqp/ScheduleAmqp.d.ts
+++ lib/Amqp/ScheduleAmqp.d.ts
@@ -1,6 +1,7 @@
 import { Schedule } from "../types";
 declare class ScheduleAmqp {
     static consume(): Promise<void>;
+    private static sleep;
     static publishDlq(schedule: Schedule): Promise<void>;
 }
 export { ScheduleAmqp };
